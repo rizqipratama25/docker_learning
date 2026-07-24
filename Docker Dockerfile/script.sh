1302f40de147269170c3a9f0a51c50c8020f6d4a
rizqipratama25 <rizqip501@gmail.com>
2026-07-24 22:15:49 +0700

    learn copy and add instructions

--- Docker Dockerfile/script.sh
+++ Docker Dockerfile/script.sh
@@ -18,3 +18,30 @@ docker container create --name command rizqipratama/command
 docker container start command
 
 docker container logs command
+
+# LABEL Instruction
+docker build -t rizqipratama/label label
+
+docker image inspect rizqipratama/label
+
+# ADD Instruction
+docker build -t rizqipratama/add add
+
+docker image inspect rizqipratama/add
+
+docker container create --name add rizqipratama/add
+
+docker container start add
+
+docker container logs add
+
+# COPY Instruction
+docker build -t rizqipratama/copy copy
+
+docker image inspect rizqipratama/copy
+
+docker container create --name copy rizqipratama/copy
+
+docker container start copy
+
+docker container logs copy
